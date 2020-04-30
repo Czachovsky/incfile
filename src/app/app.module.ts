b6e5b48f9b491369ac03057a23572a36ab3145fa
Marcin R <kontakt@mr-creations.pl>
2020-04-30 13:22:42 +0200

    header, css, grid and many components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './shared/routing/app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { FaqComponent } from './components/faq/faq.component';
+import { ArticlesComponent } from './components/articles/articles.component';
+import { RocketComponent } from './components/rocket/rocket.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { QuestionsComponent } from './components/questions/questions.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    NavigationComponent
+    NavigationComponent,
+    FaqComponent,
+    ArticlesComponent,
+    RocketComponent,
+    BannerComponent,
+    QuestionsComponent
   ],
   imports: [
     BrowserModule,
