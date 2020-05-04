79df70ceed012cc17a06d4919f5bf907c1289cf6
Marcin R <kontakt@mr-creations.pl>
2020-05-04 18:38:59 +0200

    qComponent

--- src/app/components/questions/questions.component.ts
+++ src/app/components/questions/questions.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-questions',
@@ -7,9 +7,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionsComponent implements OnInit {
 
-  constructor() { }
+  expandBarElms: any;
+
+  constructor() {
+    this.expandBarElms = [
+      {
+        name: 'Overview',
+        active: true,
+        pseudoActive: false
+      },
+      {
+        name: 'What Is a Nonprofit?',
+        active: false,
+        pseudoActive: false
+      },
+      {
+        name: 'Is a Nonprofit right for you?',
+        active: false,
+        pseudoActive: false
+      },
+      {
+        name: 'Advantages & Disadvantages',
+        active: false,
+        pseudoActive: false
+      },
+      {
+        name: 'Types of Nonprofit',
+        active: false,
+        pseudoActive: false
+      },
+      {
+        name: '501c3 Status',
+        active: false,
+        pseudoActive: false
+      },
+      {
+        name: 'How to Start a Nonprofit',
+        active: false,
+        pseudoActive: false
+      }
+    ];
+    console.log(this.expandBarElms);
+  }
 
   ngOnInit(): void {
   }
 
+  expandBarAction(index) {
+    this.expandBarElms.filter((e) => {
+      return e.active = false;
+    });
+    this.expandBarElms.forEach((e, i) => {
+      if (i < index) {
+        e.pseudoActive = true;
+      }
+    });
+    this.expandBarElms[index].active = true;
+  }
+
 }
+
+
